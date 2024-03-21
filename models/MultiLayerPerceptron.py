015822c5361490f9d48a84641b1c6c37f06a0f2c
Arian <aryan23@aut.ac.ir>
2024-03-21 01:24:43 -0400

    add: models

---
+++ models/MultiLayerPerceptron.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class MultiLayerPerceptron(nn.Module):
+
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super(MultiLayerPerceptron, self).__init__()
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = nn.Linear(hidden_dim, output_dim)
+
+
+    def forward(self, x_in, apply_softmax=False):
+        y_pred = F.relu(self.fc1(x_in))
+        y_pred = F.relu(self.fc2(y_pred))
+        y_pred = self.fc2(y_pred)
+
+        if apply_softmax:
+            y_pred = F.softmax(y_pred, dim=1)
+
+        return y_pred
